8eda0865cf53de636d1afc17d5c8fc5dbee98217
Maarten Claes <maartencls@gmail.com>
2014-12-05 19:03:48 +0100

    Added help message

--- main.go
+++ main.go
@@ -5,12 +5,39 @@ import (
 	"fmt"
 	"github.com/skratchdot/open-golang/open"
 	"net/url"
+	"os"
 	"strings"
 )
 
+var helpText = `Open a DuckDuckGo search from the command line.
+
+Usage:
+
+  duck [query]
+
+Examples:
+
+  # Default DuckDuckGo search
+  duck nikola tesla
+
+  # The '!bang' commands need to be escaped
+  duck nikola tesla \!g
+  duck duckduckgo \!gh
+  duck synonym \!t
+  duck chunky bacon \!wiki
+  duck pie town \!gmap
+  duck space cats \!i
+  duck hasselhof antivirus \!yt
+
+For all possible "!bang" commands see: https://duckduckgo.com/bang.html`
+
 func main() {
 	flag.Parse()
-	query := url.QueryEscape(strings.Join(flag.Args(), " "))
-	url := fmt.Sprintf("https://duckduckgo.com/?q=%s", query)
-	open.Run(url)
+	if len(flag.Args()) >= 1 {
+		query := url.QueryEscape(strings.Join(flag.Args(), " "))
+		url := fmt.Sprintf("https://duckduckgo.com/?q=%s", query)
+		open.Run(url)
+	} else {
+		fmt.Fprintf(os.Stderr, "%s\n\n", helpText)
+	}
 }
