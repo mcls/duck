7b1b17b06bfdb49b926002703f434df63cd03ad1
Maarten Claes <maartencls@gmail.com>
2014-12-05 18:52:59 +0100

    Encode query for url

--- main.go
+++ main.go
@@ -4,12 +4,13 @@ import (
 	"flag"
 	"fmt"
 	"github.com/skratchdot/open-golang/open"
+	"net/url"
 	"strings"
 )
 
 func main() {
 	flag.Parse()
-	query := strings.Join(flag.Args(), " ")
+	query := url.QueryEscape(strings.Join(flag.Args(), " "))
 	url := fmt.Sprintf("https://duckduckgo.com/?q=%s", query)
 	open.Run(url)
 }
